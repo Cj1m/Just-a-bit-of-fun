e838a7c078e647958ddf3f8750ed910c2baa0ef6
Chris J. Perceval-Maxwell <chris.perceval-maxwell@hotmail.com>
2014-01-26 12:06:48 +0000

    TotalLegits

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+import java.awt.Desktop;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		for(int i = 0; i < 100; i ++){ 
+			try {
+				Desktop.getDesktop().browse(new URI("http://chickenonaraft.com/"));
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (URISyntaxException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+}
